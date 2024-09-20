4a278f6599f98e8a077a65fbce144082a57724b2
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-20 12:02:33 -0300

    Add files via upload

--- loja fulano/app/(tabs)/about.tsx
+++ loja fulano/app/(tabs)/about.tsx
@@ -2,8 +2,29 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function Screem(){
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Text style={styles.txt}> 
+                ola, eu sou wesllen dhenmyz brito ferreira
+            </Text>
+
+            <Text style={styles.txt}> 
+                atualmente estou realizando curso de codificação
+            </Text>
+
+            <Text style={styles.txt}> 
+               este é o meu primeiro projeto do meu curso de TI no senac, criado com typescript em expo
+            </Text>    
+            
+            <Text style={styles.txt}> 
+               se quiser ver mais projetos que eu estou construindo veja meu github:
+               https://github.com/wdhenmyz
+            </Text>   
+
+            <Text style={styles.txt}> 
+               se quiser entrar em contato veja minhas redes sociais:
+               <Text></Text>
+               <Text></Text>
+            </Text>    
         </View>
     )
 }
@@ -13,5 +34,12 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 10,
+    },
+
+    txt: {
+        fontSize: 17,
+        textAlign: 'center',
+        margin: 10
     }
 })
