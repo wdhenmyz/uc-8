e006df85e07be6694a3229d1bdf31a7c40974424
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-24 11:49:44 -0300

    Add files via upload

--- distribuidora suplementos/app/(tabs)/menu/list.tsx
+++ distribuidora suplementos/app/(tabs)/menu/list.tsx
@@ -1,5 +1,4 @@
-import { useState, useRef } from "react";
-import { View, StyleSheet, Animated, TouchableOpacity, Image } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Image, Text } from "react-native";
 
 
 export default function Screen (){
@@ -7,13 +6,62 @@ export default function Screen (){
     return(
         <View style={styles.container}>
 
+            <View style={styles.topo}>  
+                <Image
+                    source={{uri: 'https://cdn-icons-png.flaticon.com/128/74/74472.png'}}
+                    resizeMode="cover"
+                    style={styles.topoicone}
+                />
+                <View style={styles.topocaixa}>
+                    <Text style={styles.caixatxt}> USUÁRIO: XXXXX YYYY ZZZZ</Text>
+                    <Text style={styles.caixatxt}> ID: 00-0000000.11</Text>
+                    <Text style={styles.caixatxt}> DATA: DD/MM/AAAA</Text>
+                </View>
+            </View>
+
             <TouchableOpacity style={styles.config}>
                 <Image
                     source={{uri: 'https://cdn-icons-png.flaticon.com/128/2645/2645890.png'}}
                     resizeMode="cover"
                     style={styles.IconeConfig}
                 />
-    
+                <Text style={styles.txtconfig}> Ativar notificações </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.config}>
+                <Image
+                    source={{uri: 'https://cdn-icons-png.flaticon.com/128/17317/17317821.png'}}
+                    resizeMode="cover"
+                    style={styles.IconeConfig}
+                />
+                <Text style={styles.txtconfig}> Gerenciar conta </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.config}>
+                <Image
+                    source={{uri: 'https://cdn-icons-png.flaticon.com/128/15792/15792390.png'}}
+                    resizeMode="cover"
+                    style={styles.IconeConfig}
+                />
+                <Text style={styles.txtconfig}> Gerenciar compras </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.config}>
+                <Image
+                    source={{uri: 'https://cdn-icons-png.flaticon.com/128/7595/7595902.png'}}
+                    resizeMode="cover"
+                    style={styles.IconeConfig}
+                />
+                <Text style={styles.txtconfig}> suporte ao cliente </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.config}>
+                <Image
+                    source={{uri: 'https://cdn-icons-png.flaticon.com/128/2734/2734817.png'}}
+                    resizeMode="cover"
+                    style={styles.IconeConfig}
+                />
+                <Text style={styles.txtconfig}> desconectar a conta </Text>
             </TouchableOpacity>
 
         </View>
@@ -24,9 +72,43 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#363636',
-        flexDirection: 'row', 
+        flexDirection: 'column', 
         alignItems: 'center', 
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: 6
+    },
+
+    topo:{
+        width:'98%',
+        height:'20%',
+        backgroundColor: '#B0C4DE',
+        marginBottom: 10,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        borderRadius: 10
+    },
+
+    topoicone:{
+        width: 100,
+        height: 100,
+        marginRight: 10,
+        marginLeft: 10
+    },
+
+    topocaixa:{
+        width:'69%',
+        height:'100%',
+        backgroundColor: '#B0C4DE',
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10
+    },
+
+    caixatxt:{
+        fontSize: 15,
+        marginBottom: 10,
+        marginTop: 8,
+        fontWeight: '600'
     },
 
     config: {
@@ -35,13 +117,20 @@ const styles = StyleSheet.create({
         height: 60,
         backgroundColor: '#B0C4DE',
         borderRadius: 10,
-        justifyContent: 'center'
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginBottom: 20
     },
 
     IconeConfig: {
         height: 40, 
         width: 40,
-        marginLeft: 5,
-        marginRight: 5
+        marginLeft: 10,
+        marginRight: 20
+    },
+
+    txtconfig: {
+        fontSize: 20,
+        fontWeight: 'bold',
     }
 })
