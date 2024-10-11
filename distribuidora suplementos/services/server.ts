3f3da9df4c1985e6628b98881c07872cbfd0e406
WESLLEN DHENMYZ BRITO FERREIRA <a95287227@rn.senac.local>
2024-10-11 09:35:37 -0300

    services

---
+++ distribuidora suplementos/services/server.ts
@@ -0,0 +1,41 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = await SQLite.openDatabase('loja'); 
+// abri ou cria o banco de dados
+
+// criando as tabelas se elas não exitirem
+const createTables = () => {
+    db.transaction((tx: any) => {
+
+      // criando a primeira tabela 'products'
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS products (
+          idCategory INTEGER,
+          id INTEGER PRIMARY KEY,
+          image TEXT,
+          title TEXT,
+          description TEXT,
+          price TEXT
+        );`,
+        [],
+        () => console.log('Products criada com sucesso ou já existe'),
+        (_: any, error: any) => console.log('Erro ao criar tabela: ', error)
+      );
+  
+
+      // criando a segunda tabela 'categories'
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS categories (
+          id INTEGER PRIMARY KEY,
+          title TEXT,
+          cover TEXT
+        );`,
+        [],
+        () => console.log('categories criada com sucesso ou já existe'),
+        (_: any, error: any) => console.log('Erro ao criar tabela: ', error)
+      );
+  
+    });
+  };
+  
+  export default createTables;
