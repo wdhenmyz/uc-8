e64d2a9760093228dfc953fb2e21a72c05287455
WESLLEN DHENMYZ BRITO FERREIRA <a95287227@rn.senac.local>
2024-10-14 08:20:31 -0300

    services

--- distribuidora suplementos/services/server.ts
+++ distribuidora suplementos/services/server.ts
@@ -1,42 +1,37 @@
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabaseSync('loja'); 
 // abri ou cria o banco de dados
+const db = SQLite.openDatabaseSync('loja'); 
 
 // criando as tabelas se elas não exitirem
-const createTables = () => {
-  
-    db.transaction((tx: any) => {
+const createTables = async () => {
+  try {
+    // Use execAsync to run multiple SQL commands in one go
+    await db.execAsync(`
+      PRAGMA journal_mode = WAL;  -- Setting the journal mode for better performance
 
-      // criando a primeira tabela 'products'
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS products (
-          idCategory INTEGER,
-          id INTEGER PRIMARY KEY,
-          image TEXT,
-          title TEXT,
-          description TEXT,
-          price TEXT
-        );`,
-        [],
-        () => console.log('Products criada com sucesso ou já existe'),
-        (_: any, error: any) => console.log('Erro ao criar tabela: ', error)
+      -- criando a primeira tabela 'products'
+      CREATE TABLE IF NOT EXISTS products (
+        idCategory INTEGER,
+        id INTEGER PRIMARY KEY,
+        image TEXT,
+        title TEXT,
+        description TEXT,
+        price TEXT
       );
-  
 
-      // criando a segunda tabela 'categories'
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS categories (
+      -- criando a segunda tabela 'categories'
+      CREATE TABLE IF NOT EXISTS categories (
           id INTEGER PRIMARY KEY,
           title TEXT,
           cover TEXT
-        );`,
-        [],
-        () => console.log('categories criada com sucesso ou já existe'),
-        (_: any, error: any) => console.log('Erro ao criar tabela: ', error)
       );
-  
-    });
-  };
-  
+    `);    // 
+
+    console.log('Tables created successfully');
+  } catch (error) {
+    console.error('Error creating tables:', error);
+  }
+};
+
   export default createTables;
