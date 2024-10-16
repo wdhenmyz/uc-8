b09cc0b89facd70ec363ab7b62dc0c3df664acc8
WESLLEN DHENMYZ BRITO FERREIRA <a95287227@rn.senac.local>
2024-10-16 09:29:07 -0300

    build

--- distribuidora suplementos/app/_layout.tsx
+++ distribuidora suplementos/app/_layout.tsx
@@ -10,7 +10,10 @@ export default function RootLayout(){
 
             <Stack.Screen name="(tabs)"/>
 
-            <Stack.Screen name="product/[id]" options={{headerShown: true}}/>
+            <Stack.Screen 
+                name="product/[id]" 
+                options={{
+                }}/>
 
         </Stack>
     );
