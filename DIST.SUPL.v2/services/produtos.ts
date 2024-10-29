3420ff7c7db3e1a1bc50cbae658f668a05dd90d0
WESLLEN DHENMYZ BRITO FERREIRA <a95287227@rn.senac.local>
2024-10-29 11:31:07 -0300

    DIST.SUPL.v2 > ajustando services > produtos

--- DIST.SUPL.v2/services/produtos.ts
+++ DIST.SUPL.v2/services/produtos.ts
@@ -28,11 +28,14 @@ export const getProductById = async (id: number): Promise<Products | null> => {
   try {
     const response = await fetch(`http://10.0.2.2:3000/products/${id}`);
     console.log('Response status:', response.status);
+
     if (!response.ok) {
       throw new Error('Erro ao buscar produtos');
     }
+    
     const product: Products = await response.json();
     return product;
+    
   } catch (error) {
     console.error(error);
     return null; // Retorna null em caso de erro
